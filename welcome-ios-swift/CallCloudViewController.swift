ea258cf8cf08e61877ce06015b22a0203e34f329
corinne krych <corinnekrych@gmail.com>
2016-05-12 14:35:17 +0200

    LocationViewController code

--- welcome-ios-swift/CallCloudViewController.swift
+++ welcome-ios-swift/CallCloudViewController.swift
@@ -16,21 +16,6 @@ class CallCloudViewController: UIViewController {
             menuButton.action = "revealToggle:"
             view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
-        self.cloudButton.enabled = false
-        // Initialized cloud connection
-        FH.init {(resp: Response, error: NSError?) -> Void in
-            if let error = error {
-                print("FH init failed. Error = \(error)")
-                if FH.isOnline == false {
-                    self.result.text = "Make sure you're online."
-                } else {
-                    self.result.text = "Please fill in fhconfig.plist file."
-                }
-                return
-            }
-            print("initialized OK")
-            self.cloudButton.enabled = true
-        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -41,18 +26,23 @@ class CallCloudViewController: UIViewController {
     @IBAction func callcloud(sender: UIButton) {
         print("Call cloud was clicked")
         self.cloudButton.enabled = false
-        FH.cloud("hello", method: HTTPMethod.POST,
-                 args: nil, headers: nil,
-                 completionHandler: {(resp: Response, error: NSError?) -> Void in
-                    self.cloudButton.enabled = true
-                    if let error = error {
-                        print("Cloud Call Failed, \(error)")
-                        self.result.text = "Error during Cloud call: \(error.userInfo[NSLocalizedDescriptionKey]!)"
-                        return
-                    }
-                    if let parsedRes = resp.parsedResponse as? [String:String] {
-                        self.result.text = parsedRes["msg"]
-                    }
-        })        
+        if let delegate = UIApplication.sharedApplication().delegate as? AppDelegate where !delegate.initSuccess {
+            delegate.presentAlert("Initialisation failure", message: "Relaunch the app once you fixed the configuration plist file.")
+            self.cloudButton.enabled = true
+        } else { // init is successful
+            FH.cloud("hello", method: HTTPMethod.POST,
+                     args: nil, headers: nil,
+                     completionHandler: {(resp: Response, error: NSError?) -> Void in
+                        self.cloudButton.enabled = true
+                        if let error = error {
+                            print("Cloud Call Failed, \(error)")
+                            self.result.text = "Error during Cloud call: \(error.userInfo[NSLocalizedDescriptionKey]!)"
+                            return
+                        }
+                        if let parsedRes = resp.parsedResponse as? [String:String] {
+                            self.result.text = parsedRes["msg"]
+                        }
+            })
+        }
     }
 }
